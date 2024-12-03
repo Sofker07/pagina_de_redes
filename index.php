d66b10edbf95db9ce9ae2f202b822fbf6a6afc89
Alan Paul Cortés Melchor <sofker07@gmail.com>
2024-12-02 18:23:33 -0600

    proyecto inicial

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="../pagina_redes/assets/scss/style.css">
+    <link rel="stylesheet" href="../pagina_redes/assets/css/style.css">
+    <link rel="stylesheet" href="../pagina_redes/assets/css/navbar.css">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide">
+    <title>Games</title>
+</head>
+
+<body>
+    <?php
+    // require_once ("web/navbar.php");
+    include_once('view/navbar.php');
+    ?>
+    <!-- Marca y Lema -->
+    <div class="container-header">
+        <div class="jumbotron jumbotron-fluid" style="margin-top:100px">
+            <div class="container-header">
+                <div class="col-12">
+                    <h1 class="display-4">Game Store</h1>
+                    <p class="lead">Necesitas ser el mejor jugador, por eso adquiere alguno de nuestros productos que ofrecemos, videojuegos, perifericos, hardware desde la comodidad de tu casa.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Primera vista de ventas Populares -->
+    <div class="container">
+        <div class="row ">
+            <div class="col-12 col-md-4 col-lg-3 mb-3">
+                <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 16rem;"> <img class="card-img-top" src="../pagina_redes/assets/img/Halo-Reach.png" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <p class="card-text">
+                        <ul class="list-unstyled">
+                            <li>Halo-Reach</li>
+                            <li>Halo-Reach es un juego de acción y estrategia en el que un grupo de personajes se enfrentan en un mundo hostil.</li>
+                            <a href="">Precio</a>
+                            <button type="button" class="btn btn-success">Comprar</button>
+                        </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 mb-3">
+                <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 16rem;"> <img class="card-img-top" src="../pagina_redes/assets/img/Halo-Reach.png" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <p class="card-text">
+                        <ul class="list-unstyled">
+                            <li>Halo-Reach</li>
+                            <li>Halo-Reach es un juego de acción y estrategia en el que un grupo de personajes se enfrentan en un mundo hostil.</li>
+                            <a href="">Precio</a>
+                            <button type="button" class="btn btn-success">Comprar</button>
+                        </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 mb-3">
+                <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 16rem;"> <img class="card-img-top" src="../pagina_redes/assets/img/Halo-Reach.png" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <p class="card-text">
+                        <ul class="list-unstyled">
+                            <li>Halo-Reach</li>
+                            <li>Halo-Reach es un juego de acción y estrategia en el que un grupo de personajes se enfrentan en un mundo hostil.</li>
+                            <a href="">Precio</a>
+                            <button type="button" class="btn btn-success">Comprar</button>
+                        </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 mb-3">
+                <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 16rem;"> <img class="card-img-top" src="../pagina_redes/assets/img/Halo-Reach.png" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <p class="card-text">
+                        <ul class="list-unstyled">
+                            <li>Halo-Reach</li>
+                            <li>Halo-Reach es un juego de acción y estrategia en el que un grupo de personajes se enfrentan en un mundo hostil.</li> <a href="">Precio</a>
+                            <button type="button" class="btn btn-success">Comprar</button>
+                        </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 mb-3">
+                <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 16rem;"> <img class="card-img-top" src="../pagina_redes/assets/img/Halo-Reach.png" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <p class="card-text">
+                        <ul class="list-unstyled">
+                            <li>Halo-Reach</li>
+                            <li>Halo-Reach es un juego de acción y estrategia en el que un grupo de personajes se enfrentan en un mundo hostil.</li> <a href="">Precio</a>
+                            <button type="button" class="btn btn-success">Comprar</button>
+
+                        </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 mb-3">
+                <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 16rem;"> <img class="card-img-top" src="../pagina_redes/assets/img/Halo-Reach.png" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <p class="card-text">
+                        <ul class="list-unstyled">
+                            <li>Halo-Reach</li>
+                            <li>Halo-Reach es un juego de acción y estrategia en el que un grupo de personajes se enfrentan en un mundo hostil.</li> <a href="">Precio</a>
+                            <button type="button" class="btn btn-success">Comprar</button>
+
+                        </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 mb-3">
+                <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 16rem;"> <img class="card-img-top" src="../pagina_redes/assets/img/Halo-Reach.png" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <p class="card-text">
+                        <ul class="list-unstyled">
+                            <li>Halo-Reach</li>
+                            <li>Halo-Reach es un juego de acción y estrategia en el que un grupo de personajes se enfrentan en un mundo hostil.</li> <a href="">Precio</a>
+                            <button type="button" class="btn btn-success">Comprar</button>
+
+                        </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 16rem;"> <img class="card-img-top" src="../pagina_redes/assets/img/Halo-Reach.png" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <p class="card-text">
+                        <ul class="list-unstyled">
+                            <li>Halo-Reach</li>
+                            <li>Halo-Reach es un juego de acción y estrategia en el que un grupo de personajes se enfrentan en un mundo hostil.</li> <a href="">Precio</a>
+                            <button type="button" class="btn btn-success">Comprar</button>
+
+                        </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Contenedor informativo-->
+
+    <div class="container shadow p-3 mb-5 bg-body rounded h-50" >
+        <div class="row align-items-md-stretch">
+            <div class="col-md-6">
+                <div class="h-100 p-5 text-bg-info rounded-3">
+                    <h2>Game Store</h2>
+                    <p>Game Store es una tienda en línea dedicada a los videojuegos, donde encontrarás una amplia variedad de títulos y accesorios de las mejores marcas, perfectos para mejorar tu experiencia de juego. Nuestro catálogo cubre todas las plataformas y estilos de juegos, desde los clásicos hasta los últimos lanzamientos. Aprovecha las promociones exclusivas, descuentos, y cupones disponibles para todos los gustos. ¡Regístrate ahora y lleva tu experiencia gaming al siguiente nivel!</p>
+
+                    <button class="btn btn-outline-light shadow p-3 mb-5 bg-body rounded" type="button">Registrate</button>
+                </div>
+            </div>
+            <div class="col-md-6 h-50" >
+                <div class="h-50 p-5 bg-light border rounded-3">
+                    <img src="../pagina_redes/assets/img/logo_negro_h.png" alt="" srcset="" class="card-img-top" >
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Footer -->
+    <div class="container-footer">
+        <footer>
+            <div class="logo-footer">
+                <img src="../pagina_redes/assets/img/logo_negro_v.png" alt="">
+            </div>
+            <div class="redes-footer">
+                <a
+                    href="https://scontent.fpbc1-2.fna.fbcdn.net/v/t39.30808-6/416217375_2411911555658186_6334468771007309534_n.jpg?stp=dst-jpg_p180x540&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGP1mUlEctb0w9Bd21nNsJeEGVKb3IcvTkQZUpvchy9OTFZORzuycoHVfvQoXO5gX3bin1_IuBi1f7ECletfwx6&_nc_ohc=6yRzEF1SgooQ7kNvgE9K1DA&_nc_ht=scontent.fpbc1-2.fna&cb_e2o_trans=t&oh=00_AYDnY1zzzy_nFY84wfraf_iC0jmvSlkgO2juojU56Lju1A&oe=6667C5F4"><i
+                        class="fab fa-facebook-f icon-redes-footer"></i></a>
+                <a href="#"><i class="fab fa-google-plus-g icon-redes-footer"></i></a>
+                <a href="#"><i class="fab fa-instagram icon-redes-footer"></i></a>
+            </div>
+            <hr>
+            <h4>© 2024 GAME STORE</h4>
+            <p>GAME STORE</p>
+        </footer>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- SCRIPTS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
---
+++ view/navbar.php
@@ -0,0 +1,67 @@
+    <div class="container-fluid">
+        <nav class="navbar navbar-dark bg-dark fixed-top">
+            <div class="container-fluid">
+            <img class="logo" src="../pagina_redes/assets/img/logo_blanco_h.png" alt="">
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Games</h5>
+                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Plataform
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-joystick" viewBox="0 0 16 16">
+                                        <path d="M10 2a2 2 0 0 1-1.5 1.937v5.087c.863.083 1.5.377 1.5.726 0 .414-.895.75-2 .75s-2-.336-2-.75c0-.35.637-.643 1.5-.726V3.937A2 2 0 1 1 10 2" />
+                                        <path d="M0 9.665v1.717a1 1 0 0 0 .553.894l6.553 3.277a2 2 0 0 0 1.788 0l6.553-3.277a1 1 0 0 0 .553-.894V9.665c0-.1-.06-.19-.152-.23L9.5 6.715v.993l5.227 2.178a.125.125 0 0 1 .001.23l-5.94 2.546a2 2 0 0 1-1.576 0l-5.94-2.546a.125.125 0 0 1 .001-.23L6.5 7.708l-.013-.988L.152 9.435a.25.25 0 0 0-.152.23" />
+                                    </svg>
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-dark">
+                                    <li><a class="dropdown-item" href="#">PC</a></li>
+                                    <li><a class="dropdown-item" href="#">Xbox</a></li>
+                                    <li><a class="dropdown-item" href="#">Playstation</a></li>
+                                    <li><a class="dropdown-item" href="#">Nintendo</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Perifericos & Accesorios
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-dark">
+                                    <li>
+                                        <a class="dropdown-item" href="#">Razer</a>
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Turtle Beach</a></li>
+                                    <li><a class="dropdown-item" href="#">Corsair</a></li>                                    
+                                    <li><a class="dropdown-item" href="#">Logitech</a></li>
+                                    <li><a class="dropdown-item" href="#">Xbox</a></li>
+                                    <li><a class="dropdown-item" href="#">Playstation</a></li>
+                                    <li><a class="dropdown-item" href="#">Nintendo</a></li>
+                                    <li><a class="dropdown-item" href="#">Asus</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Otros</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <form class="d-flex mt-3" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+    
